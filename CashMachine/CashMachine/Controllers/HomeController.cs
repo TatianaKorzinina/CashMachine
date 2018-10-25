6c7ce6468fe47146ffb93c33973da39e074b5bcd
Tatiana Korzinina <sambara@yandex.ru>
2018-10-25 16:05:30 +0300

    Error Messages added

--- CashMachine/CashMachine/Controllers/HomeController.cs
+++ CashMachine/CashMachine/Controllers/HomeController.cs
@@ -17,37 +17,34 @@ namespace CashMachine.Controllers
             _logger = logger;
         }
 
-        // GET api/values
+        // GET api/home
         [HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
         public Account Get()
         {
             Repository repository = new Repository();
             return repository.Balance();
         }
-        // GET api/values/5
-        [HttpGet("{id:min(100)}")]
-        public JsonResult Get(int id)
+
+        // GET api/home/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
         {
            
             Repository repository = new Repository();
             //if (id % 100 != 0) yield return "введите сумму кратную 100 рублям";
-            if (repository.Balance().Amount >= id && id % 100 == 0)
+            if (repository.Balance().Amount >= id && id>=100 && id % 100 == 0)
             {
                 Atm atm = new Atm();
 
                 atm.money =  repository.GetMoney(id);
-                return Json(atm);
+                return Ok(atm);
 
                 //foreach (var item in money)
                 //{
                 //    yield return $"по {item.Key}  -- {item.Value} купюр";
                 //}
             }
-            else return Json((object)"на вашем счете недостаточно средств");
+            else return BadRequest(new Errors { ErrorMessage= "сумма должа быть кратна 100 и не превышать ваш баланс" });
 
             
         }
---
+++ CashMachine/CashMachine/Errors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashMachine
+{
+    public class Errors
+    {
+        //public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
