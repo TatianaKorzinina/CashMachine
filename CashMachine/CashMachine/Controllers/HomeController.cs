1c0ccbcfb82b330d388976ff9356a41fffbdbe27
Tatiana Korzinina <sambara@yandex.ru>
2018-10-25 13:01:01 +0300

    logs added

--- CashMachine/CashMachine/Controllers/HomeController.cs
+++ CashMachine/CashMachine/Controllers/HomeController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CashMachine.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET api/values
         [HttpGet]
         //public IEnumerable<string> Get()
